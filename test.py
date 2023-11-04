d5cf224ab01d294a1c05f73b833ea3877812afa5
LernerW <1121430447@qq.com>
2023-11-05 00:19:38 +0800

    [test]change mock to subprocess

--- test.py
+++ test.py
@@ -2,230 +2,235 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 import os
+import shutil
+import subprocess
 
 import main
 
-output_template = 'Session is over\n'
+output_template = "Session is over\n"
+
 
 class TestCLI(unittest.TestCase):
     def setUp(self):
+        shutil.rmtree("test_dir", ignore_errors=True)
         # create test directory
         try:
-            os.mkdir('test_dir')
+            os.mkdir("test_dir")
         except:
-            pass
+            print("mkdir test_dir failed")
         # switch to test directory
-        os.chdir('test_dir')
-    
+        os.chdir("test_dir")
+
     def tearDown(self):
         # switch to parent directory
-        os.chdir('..')
+        os.chdir("..")
         # remove test directory
         try:
-            os.rmdir('test_dir')
+            shutil.rmtree("test_dir")
         except:
-            pass
-    
-    @patch('builtins.input', side_effect=['quit'])
-    def test_quit_command(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
-        expected_output = '' + output_template
-        self.assertIn(expected_output, output.getvalue())
-    
+            print("rm test_dir failed")
+
+    def test_quit_command(self):
+        command_list = ["quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
+        with open("outputs.txt", "r") as f:
+            output = f.read()
+
+        expected_output = "" + output_template
+        self.assertIn(expected_output, output)
+
     # load existing file
-    @patch('builtins.input', side_effect=['load test.md', 'quit'])
-    def test_load_1(self, input):
+    def test_load_1(self):
         # create a file
-        with open('test.md', 'w') as f:
-            f.write('test')
-        
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+        with open("test.md", "w") as f:
+            f.write("test")
 
-        with open('test.md', 'r') as f:
+        command_list = ["load test.md", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
+
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'test')
-    
+        self.assertEqual(content, "test")
+
     # load non-existing file
-    @patch('builtins.input', side_effect=['load test.md', 'save', 'quit'])
-    def test_load_one_file_save(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_load_one_file_save(self):
+        command_list = ["load test.md", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
         # check whether the file is created
-        files = os.listdir('.')
-        # delete the file
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertIn('test.md', files)
-    
+        files = os.listdir(".")
+        self.assertIn("test.md", files)
+
     # load two non-existing file
-    @patch('builtins.input', side_effect=['load test.md', 'save', 'load test_2.md', 'save', 'quit'])
-    def test_load_two_file_save(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_load_two_file_save(self):
+        command_list = ["load test.md", "save", "load test_2.md", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
         # check whether the file is created
-        files = os.listdir('.')
-        # delete the file
-        try:
-            os.remove('test.md')
-            os.remove('test_2.md')
-        except:
-            pass
-        self.assertIn('test.md', files)
-        self.assertIn('test_2.md', files)
-    
-    # @patch('builtins.input', side_effect=['load ./test.md', 'save', 'quit'])
-    # def test_load_current_dir_file(self, input):
-    #     with patch('sys.stdout', new=StringIO()) as output:
-    #         main.run()
+        files = os.listdir(".")
+        self.assertIn("test.md", files)
+        self.assertIn("test_2.md", files)
 
-    #     # check whether the file is created
-    #     files = os.listdir('.')
-    #     # delete the file
-    #     try:
-    #         os.remove('test.md')
-    #     except:
-    #         pass
-    #     self.assertIn('test.md', files)
-    
     # insert one line
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'save', 'quit'])
-    def test_insert_1(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_1(self):
+        command_list = ["load test.md", "insert a", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'a')
-    
+        self.assertEqual(content, "a")
+
     # insert two lines
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'insert b', 'save', 'quit'])
-    def test_insert_2(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_2(self):
+        command_list = ["load test.md", "insert a", "insert b", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'a\nb')
-    
+        self.assertEqual(content, "a\nb")
+
     # insert two lines with position
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'insert 1 b', 'save', 'quit'])
-    def test_insert_3(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_3(self):
+        command_list = ["load test.md", "insert a", "insert 1 b", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'b\na')
-    
+        self.assertEqual(content, "b\na")
+
     # insert three lines with position
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'insert 1 b', 'insert 1 c', 'save', 'quit'])
-    def test_insert_4(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_4(self):
+        command_list = [
+            "load test.md",
+            "insert a",
+            "insert 1 b",
+            "insert 1 c",
+            "save",
+            "quit",
+        ]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'c\nb\na')
-    
+        self.assertEqual(content, "c\nb\na")
+
     # overflow line insert
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'insert 3 b', 'save', 'quit'])
-    def test_insert_5(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_5(self):
+        command_list = ["load test.md", "insert a", "insert 3 b", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'a\nb')
-    
+        self.assertEqual(content, "a\nb")
+
     # insert line to existing file
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'save', 'load test.md', 'insert 1 b', 'save', 'quit'])
-    def test_insert_6(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_insert_6(self):
+        command_list = [
+            "load test.md",
+            "insert a",
+            "save",
+            "load test.md",
+            "insert 1 b",
+            "save",
+            "quit",
+        ]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'b\na')
+        self.assertEqual(content, "b\na")
 
     # append head once
-    @patch('builtins.input', side_effect=['load test.md', 'append-head a', 'save', 'quit'])
-    def test_append_head_1(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_append_head_1(self):
+        command_list = ["load test.md", "append-head a", "save", "quit"]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'a')
-    
+        self.assertEqual(content, "a")
+
     # append head twice
-    @patch('builtins.input', side_effect=['load test.md', 'append-head a', 'append-head b', 'save', 'quit'])
-    def test_append_head_2(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_append_head_2(self):
+        command_list = [
+            "load test.md",
+            "append-head a",
+            "append-head b",
+            "save",
+            "quit",
+        ]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'b\na')
-    
+        self.assertEqual(content, "b\na")
+
     # append head once
-    @patch('builtins.input', side_effect=['load test.md', 'insert a', 'append-head b', 'save', 'quit'])
-    def test_append_head_3(self, input):
-        with patch('sys.stdout', new=StringIO()) as output:
-            main.run()
+    def test_append_head_3(self):
+        command_list = [
+            "load test.md",
+            "append-head a",
+            "append-head b",
+            "save",
+            "quit",
+        ]
+        with open("inputs.txt", "w") as f:
+            f.write("\n".join(command_list))
+        with open("inputs.txt", "r") as f:
+            with open("outputs.txt", "w") as out:
+                subprocess.call(["python", "../main.py"], stdin=f, stdout=out)
 
-        with open('test.md', 'r') as f:
+        with open("test.md", "r") as f:
             content = f.read()
-        try:
-            os.remove('test.md')
-        except:
-            pass
-        self.assertEqual(content, 'b\na')
-    
+        self.assertEqual(content, "b\na")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
